8c0665b38c1c3f815bb1bd69c3d8066639039ec7
Nick Banks <nibanks@microsoft.com>
2023-04-25 10:26:56 -0400

    Use RIO

--- src/platform/datapath_winuser.c
+++ src/platform/datapath_winuser.c
@@ -1869,7 +1869,7 @@ CxPlatSocketCreateUdp(
     Socket->ClientContext = Config->CallbackContext;
     Socket->HasFixedRemoteAddress = (Config->RemoteAddress != NULL);
     Socket->Type = CXPLAT_SOCKET_UDP;
-    Socket->UseRio = FALSE;
+    Socket->UseRio = TRUE;
     if (Config->LocalAddress) {
         CxPlatConvertToMappedV6(Config->LocalAddress, &Socket->LocalAddress);
     } else {
